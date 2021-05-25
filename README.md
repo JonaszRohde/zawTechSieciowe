98d3e8be38f2306f73560ff6f608425b7d19c947
JonaszRohde <63390272+JonaszRohde@users.noreply.github.com>
2021-05-25 17:26:42 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# zawTechSieciowe
+Aplikacja “Przepisy Babci” służy do wyświetlania przepisów potraw.
+
+
+Użyte API
+
+https://developer.edamam.com/edamam-recipe-api
+
+Twórcy
+
+Jonasz Rohde 51106
+Dawid Krefft 50921
---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import Recipe from "./components/Recipe";
+import Alert from "./components/Alert";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
+
+  const APP_ID = "6fcdb977";
+  const APP_KEY = "aedf1229dd579cbd0507dd1c293d59bb";
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const getData = async () => {
+    if (query !== "") {
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  };
+
+  const onChange = e => setQuery(e.target.value);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    getData();
+  };
+
+  return (
+    <div className="App">
+      <h1>Przepisy Babci ♥</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="recipes">
+        {recipes !== [] &&
+          recipes.map(recipe => <Recipe key={uuidv4()} recipe={recipe} />)}
+      </div>
+    </div>
+  );
+}
+
+export default App;
